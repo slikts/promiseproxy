44a6b1f01369d335f1a40d34b10645d10d40c0ce
slikts <dabas@untu.ms>
2016-05-24 21:10:36 +0300

    test

---
+++ test/promiseproxy.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const test = require('tape')
+const {PromiseProxy} = require('../')
+
+test('instatiate: with new', function (t) {
+  const schema = {
+    tabs: {
+      query: 1
+    }
+  }
+
+  const n = 123
+  // mock
+  const _chrome = {
+    tabs: {
+      query: (_, callback) => {
+        setTimeout(() => callback(n), 0)
+      }
+    }
+  }
+
+  const pchrome = PromiseProxy(_chrome, schema)
+
+  pchrome.tabs.query({a: 1}).then((arg) => {
+    t.equal(arg, n)
+    t.end()
+  })
+})
